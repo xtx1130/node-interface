b90b7f35ad2ecf387a339b3078b9e70918b1e4ff
xtx1130 <xtx1130@gmail.com>
2017-04-28 14:51:47 +0800

    test pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,21 @@
 'use strict';
 let env = process.env.NODE_ENV || 'development';
 let watch = env==='development'?true:false;
+let port = void 0;
+console.log(process.argv);
+for (let i in process.argv){
+  if(process.argv[i].match('port'))
+    port = process.argv[i].split('=')[1]
+}
 module.exports = {
 	apps:[{
 		name:'interface',
 		script:'./index.js',
 		watch:watch,
 		instance:1,
-		log_date_format:'YYYY-MM-DD HH'
+		log_date_format:'YYYY-MM-DD HH',
+    exec_mode : 'cluster',
+    nodeArgs: "port="+port
 		//out_file:$HOME/.pm2/logs/XXXout.log,
 	}]
 }
